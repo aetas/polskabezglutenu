523f7b73cef0e881c26847618ba6f97063998598
Mariusz Józala <mariusz@aetas.pl>
2025-05-03 23:49:43 +0200

    Zmiana komponentu do wyboru miasta na Combobox z możliwością szybkiego filtrowania

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -8,6 +8,22 @@ import {
   SelectTrigger,
   SelectValue,
 } from "./ui/select";
+import {
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+} from "@/components/ui/command"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+import {Button} from "@/components/ui/button.tsx";
+import {Check, ChevronDown} from "lucide-react";
+import {cn} from "@/lib/utils.ts";
 
 interface FilterProps {
   onFilterChange: (filters: {
@@ -25,6 +41,7 @@ export function Filter({ onFilterChange, categories, cities }: FilterProps) {
   const [category, setCategory] = useState("*");
   const [city, setCity] = useState("*");
   const [glutenFree, setGlutenFree] = useState(false);
+  const [openCity, setOpenCity] = useState(false);
 
   useEffect(() => {
       onFilterChange({
@@ -58,22 +75,67 @@ export function Filter({ onFilterChange, categories, cities }: FilterProps) {
           ))}
         </SelectContent>
       </Select>
-      <Select
-        value={city}
-        onValueChange={(value) => setCity(value)}
-      >
-        <SelectTrigger>
-          <SelectValue placeholder="Miejscowość" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="*">Wszystkie miejscowości</SelectItem>
-          {cities.map((c) => (
-            <SelectItem key={c} value={c}>
-              {c}
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
+      <Popover open={openCity} onOpenChange={setOpenCity}>
+          <PopoverTrigger asChild>
+              <Button
+                  variant="outline"
+                  role="combobox"
+                  aria-expanded={openCity}
+                  className="w-[230px] justify-between"
+                  value={city}
+
+              >
+                  {city != "*"
+                      ? cities.find((cities1) => cities1 === city)
+                      : "Wszystkie miejscowości"}
+                  <ChevronDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+              </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-[230px] p-0">
+              <Command>
+                  <CommandInput placeholder="Znajdź miejscowość..." />
+                  <CommandList>
+                      <CommandEmpty>Nie znaleziono</CommandEmpty>
+                      <CommandGroup>
+                          <CommandItem
+                              key="*"
+                              value="*"
+                              onSelect={() => {
+                                  setCity("*")
+                                  setOpenCity(false)
+                              }}
+                          >
+                              <Check
+                                  className={cn(
+                                      "mr-2 h-4 w-4",
+                                      "*" === city ? "opacity-100" : "opacity-0"
+                                  )}
+                              />
+                              Wszystkie miejscowości
+                          </CommandItem>
+                          {cities.map((value) => (
+                              <CommandItem
+                                  key={value}
+                                  value={value}
+                                  onSelect={(currentValue) => {
+                                      setCity(currentValue)
+                                      setOpenCity(false)
+                                  }}
+                              >
+                                  <Check
+                                      className={cn(
+                                          "mr-2 h-4 w-4",
+                                          value === city ? "opacity-100" : "opacity-0"
+                                      )}
+                                  />
+                                  {value}
+                              </CommandItem>
+                          ))}
+                      </CommandGroup>
+                  </CommandList>
+              </Command>
+          </PopoverContent>
+      </Popover>
       <Toggle
         pressed={glutenFree}
         onPressedChange={(value) => setGlutenFree(value)}
